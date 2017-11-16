7f00e533e1cd286e3dd86c88fc3ce8031500943c
Ross <rosssullivan101@gmail.com>
2017-11-15 18:17:42 -0600

    Reorganized Directories

---
+++ Java Module/ex/ex4/GraphPanel.java
@@ -0,0 +1,115 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class RandomClassForTesting{
+    GraphPanel var = new  GraphPanel();
+
+    int one = var.one();
+
+}
+
+class GraphPanel extends JPanel implements Serializable {
+    ArrayList<Line> lines = new ArrayList<>();
+    ArrayList<Text> texts = new ArrayList<>();//tdhf
+
+    int helloclass = 0;
+
+    transient Graphics gr=null;
+    public static int width= 0;
+    public static int height=0;//sdfsdfsdf
+
+    public GraphPanel() {                                       //jugfhj
+        if (a == b) {
+        }
+    }
+
+    public static int one(){
+        return 1;
+    }
+
+    public void setTextSize(int i) {
+
+    }
+
+
+    //c class gr.setFont(new Font("TimesRoman",Font.PLAIN, 11));
+
+
+
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    int q = 45;/*olskGN///D/DNFDFNB*/
+    int x = 346;/*jshdgusdivgbsv*/
+    int y = 35;
+
+
+    public void addLine(Line l) {
+
+        RandomClassForTesting ran = new RandomClassForTesting();
+        int someInt = 1;
+
+        ran.one = 5;
+        someInt = 5;
+
+        lines.add(l);
+    }
+
+    public void addText(Text t) {
+        texts.add(t);
+    }
+
+    public void paintComponent(Graphics g) {
+        gr = g;
+        setBackground(Color.WHITE);
+
+
+        super.paintComponent(g);
+        width = getWidth();
+
+
+        height = getHeight();//dfghjkg
+        g.setColor(Color.black);
+
+
+        for (Line l : lines) {
+            if (l.isDashed()) {
+                Graphics2D g2d = (Graphics2D) g.create();
+
+
+                Stroke dashed = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+                g2d.setStroke(dashed);
+                g2d.drawLine(l.x1, l.y1, l.x2, l.y2);
+//gfhjfgh
+            } else
+                g.drawLine(l.x1, l.y1, l.x2, l.y2);
+        }
+
+        for (Text t : texts) {
+            if (t.text != null) {
+                Rectangle r = t.getBounds();
+                //g.drawRect(r.x,r.y,r.width,r.height);
+                t.setBounds(g);
+                int p = Font.PLAIN;
+                if (t.bold)
+                    p = Font.BOLD;
+                g.setColor(t.color);
+                g.setFont(new Font("TimesRoman", p, t.size));
+                g
+                        .
+                                drawString(t.text, t.x, t.y);
+            }
+        }//jfjhfhj
+    }
+
+
+    public void clear() {
+        lines.clear();
+        texts.clear();
+    }
+}
