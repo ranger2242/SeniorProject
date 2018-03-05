bd76e6df249d4a10a2ed992ab6fd7f36ea3ea616
Ross <rosssullivan101@gmail.com>
2018-03-05 15:06:45 -0600

    Updated todo list, added python loading bar

--- Python Module/SOMObject.py
+++ Python Module/SOMObject.py
@@ -5,6 +5,9 @@ import math
 import sys
 from Helpers import get_python_project_root
 from Helpers import slog
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 class SOM(object):
@@ -13,7 +16,7 @@ class SOM(object):
 
     def __init__(self, m, n, dim, n_iterations=100, alpha=None, sigma=None, model_name='model.ckpt'):
 
-        slog('Creating Map...')
+        slog('\t--- Creating Map ---')
 
         # Assign required variables first
         self.m = m
@@ -38,20 +41,17 @@ class SOM(object):
         with self.graph.as_default():
 
             # To save data, create weight vectors and their location vectors
-
             self.weightage_vects = tf.Variable(tf.random_normal([m * n, dim]), name='weightage_vects')
 
             self.location_vects = tf.constant(np.array(list(self.neuron_locations(m, n))), name='location_vects')
 
             # Training inputs
-
             # The training vector
             self.vect_input = tf.placeholder("float", [dim], name='vect_input')
             # Iteration number
             self.iter_input = tf.placeholder("float", name='iter_input')
 
             # Training Operation  # tf.stack result will be [ (m*n),  dim ]
-
             bmu_index = tf.argmin(tf.sqrt(tf.reduce_sum(
                 tf.pow(tf.subtract(self.weightage_vects, tf.stack(
                     [self.vect_input for _ in range(m * n)])), 2), 1)), 0)
@@ -60,22 +60,47 @@ class SOM(object):
             bmu_loc = tf.reshape(
                 tf.slice(self.location_vects, slice_input, tf.constant(np.array([1, 2]), dtype=tf.int64)), [2])
 
+
             # To compute the alpha and sigma values based on iteration number
             learning_rate_op = tf.subtract(1.0, tf.div(self.iter_input, self.n_iterations))
             alpha_op = tf.multiply(alpha, learning_rate_op)
             sigma_op = tf.multiply(sigma, learning_rate_op)
 
+
             # learning rates for all neurons, based on iteration number and location w.r.t. BMU.
             bmu_distance_squares = tf.reduce_sum(tf.pow(tf.subtract(
                 self.location_vects, tf.stack([bmu_loc for _ in range(m * n)])), 2), 1)
+
 
             neighbourhood_func = tf.exp(tf.negative(tf.div(tf.cast(
                 bmu_distance_squares, "float32"), tf.pow(sigma_op, 2))))
             learning_rate_op = tf.multiply(alpha_op, neighbourhood_func)
 
             # Finally, the op that will use learning_rate_op to update the weightage vectors of all neurons
-            learning_rate_multiplier = tf.stack([tf.tile(tf.slice(
-                learning_rate_op, np.array([i]), np.array([1])), [dim]) for i in range(m * n)])
+            to_stack = []
+            loading_seg = int((m * n) / 10)
+            for i in range(m*n):
+                to_stack.append(tf.tile(tf.slice(learning_rate_op, np.array([i]), np.array([1])), [dim]))
+
+                if (i + 1) % loading_seg == 0:
+
+                    num_stars = int((i + 1) / loading_seg)
+                    message = '\t['
+
+                    for j in range(num_stars):
+                        message += '*'
+                    for j in range(14 - num_stars):
+                        message += ' '
+
+                    message += ']'
+
+                    slog(message, replace=True)
+
+            learning_rate_multiplier = tf.stack(to_stack)
+
+            # Calculate learning rate Backup
+            # learning_rate_multiplier = tf.stack([tf.tile(tf.slice(
+            #     learning_rate_op, np.array([i]), np.array([1])), [dim]) for i in range(m * n)])
 
             ### Strucutre of updating weight ###
             ### W(t+1) = W(t) + W_delta ###
@@ -85,24 +110,27 @@ class SOM(object):
             weightage_delta = tf.multiply(
                 learning_rate_multiplier,
                 tf.subtract(tf.stack([self.vect_input for _ in range(m * n)]), self.weightage_vects))
-
+            slog('[***********   ]', replace=True)
             # W(t+1) = W(t) + W_delta
             new_weightages_op = tf.add(self.weightage_vects, weightage_delta)
-
+            slog('[************  ]', replace=True)
             # Update weightge_vects by assigning new_weightages_op to it.
             self.training_op = tf.assign(self.weightage_vects, new_weightages_op)
-
             self.sess = tf.Session()
             self.saver = tf.train.Saver()
-
+            slog('[************* ]', replace=True)
             try:
                 self.saver.restore(self.sess, 'tmp\\' + self.model_name)
                 self.store_centroid_grid()
                 self.trained = True
+                slog('[**************]', replace=True)
+                print()
                 slog('Model loaded from checkpoint')
             except:
                 init_op = tf.global_variables_initializer()
                 self.sess.run(init_op)
+                slog('[**************]', replace=True)
+                print()
 
     def neuron_locations(self, m, n):
         for i in range(m):
